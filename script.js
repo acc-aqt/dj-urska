45a0b1d010959f3082ab972cd5153ea05bddae46
alexander-carrels <alexander.carrels@outlook.com>
2026-06-11 22:53:16 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const videos = [
+  {
+    title: "Dj Urška performing live at 'Klub K4' in Ljubljana, Slovenia -  06/2027",
+    src: "assets/video1.mp4"
+  },
+  {
+    title: "Dj Urška performing live at Rote Sonne at 'Rote Sonne' in Munich, Germany -  08/2027",
+    src: "assets/video2.mp4"
+  },
+];
+
+const video = document.getElementById("video");
+const videoTitle = document.getElementById("videoTitle");
+const nextBtn = document.getElementById("nextBtn");
+const muteBtn = document.getElementById("muteBtn");
+
+let currentIndex = -1;
+
+function playRandomVideo() {
+  let newIndex;
+
+  do {
+    newIndex = Math.floor(Math.random() * videos.length);
+  } while (videos.length > 1 && newIndex === currentIndex);
+
+  currentIndex = newIndex;
+
+  video.src = videos[currentIndex].src;
+  videoTitle.textContent = videos[currentIndex].title;
+
+  video.play();
+}
+
+nextBtn.addEventListener("click", playRandomVideo);
+
+muteBtn.addEventListener("click", () => {
+  video.muted = !video.muted;
+  muteBtn.textContent = video.muted ? "Club Mode" : "Respect the neighbors";
+});
+
+window.addEventListener("DOMContentLoaded", playRandomVideo);
