1aea943941d81f756d8fb3d82f5c3c09d53acc2f
alexander-carrels <alexander.carrels@outlook.com>
2026-06-12 23:34:08 +0200

    Layout update with subtitle

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 const videos = [
   {
-    title: "Dj Urška performing live at 'Klub K4' in Ljubljana, Slovenia -  06/2027",
+    title: "Dj Urška performing live at 'Klub K4'",
+    subtitle: "Ljubljana, Slovenia -  06/2027",
     src: "assets/video1.mp4"
   },
   {
-    title: "Dj Urška leaving stage at 'Butik festical' in Tolmin, Slovenia -  07/2027",
+    title: "Dj Urška leaving stage at 'Butik festical'",
+    subtitle: "Tolmin, Slovenia -  07/2027",
     src: "assets/video4.mp4"
   },
   {
-    title: "Leaked recording from 'Berghain' in Berlin, Germany -  09/2027",
+    title: "Leaked recording from 'Berghain'",
+    subtitle: "Berlin, Germany -  09/2027",
     src: "assets/video3.mp4"
   },
   {
-    title: "Dj Urška performing live at 'Rote Sonne' in Munich, Germany -  08/2027",
+    title: "Dj Urška performing live at 'Rote Sonne'",
+    subtitle: "Munich, Germany -  08/2027",
     src: "assets/video2.mp4"
   },
   {
-    title: "Dj Urška performing live at 'Womb' in Tokyo, Japan -  09/2027",
+    title: "Dj Urška performing live at 'Womb'",
+    subtitle: "Tokyo, Japan -  09/2027",
     src: "assets/video5.mp4"
   },
   ,
   {
-    title: "Dj Urška performing live at 'Fabric' in London, England -  10/2027",
+    title: "Dj Urška performing live at 'Fabric'",
+    subtitle: "London, England -  10/2027",
     src: "assets/video6.mp4"
   },
   {
-    title: "Dj Urška performing live at 'Brookyln Mirage' in New York, USA -  11/2027",
+    title: "Dj Urška performing live at 'Brookyln Mirage'",
+    subtitle: "Brooklyn, USA -  11/2027",
     src: "assets/video7.mp4"
   },
 ];
 
 const video = document.getElementById("video");
 const videoTitle = document.getElementById("videoTitle");
+const videoSubtitle = document.getElementById("videoSubtitle");
 const nextBtn = document.getElementById("nextBtn");
 const muteBtn = document.getElementById("muteBtn");
 
@@ -48,6 +56,8 @@ function playRandomVideo() {
 
   video.src = videos[currentIndex].src;
   videoTitle.textContent = videos[currentIndex].title;
+
+videoSubtitle.textContent = videos[currentIndex].subtitle;
 
   video.play();
 }
